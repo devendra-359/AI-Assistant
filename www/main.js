7187534f1749f6228d221833829cb8256f4b96bd
Devendra <dvaraprasad367@gmail.com>
2025-07-31 15:22:47 +0530

    Initial commit

---
+++ www/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.tlt').textillate({
+    loop: true,
+    sync: true,
+    in: {
+      effect: "bounceIn"
+    },
+    out: {
+      effect: "bounceOut"
+    }
+  });
+});
